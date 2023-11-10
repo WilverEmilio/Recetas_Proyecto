25fd999bd76bb80eae6ed15f70b4914eb2db6b67
Hanz Maverick <maverick_ruiz@mesoamericana.edu.gt>
2023-11-09 21:04:44 -0600

    Cambios a nivel de back y front

--- recetas/api/Progra/Recetas.php
+++ recetas/api/Progra/Recetas.php
@@ -1,42 +1,10 @@
 <?php
-/*
-
-  ____          _____               _ _           _       
- |  _ \        |  __ \             (_) |         | |      
- | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
- |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
- | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
- |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
-         __/ |                               __/ |        
-        |___/                               |___/         
-    
-____________________________________
-/ Si necesitas ayuda, contáctame en \
-\ https://parzibyte.me               /
- ------------------------------------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
-                ||----w |
-                ||     ||
-Creado por Parzibyte (https://parzibyte.me).
-------------------------------------------------------------------------------------------------
-            | IMPORTANTE |
-Si vas a borrar este encabezado, considera:
-Seguirme: https://parzibyte.me/blog/sigueme/
-Y compartir mi blog con tus amigos
-También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
-Twitter: https://twitter.com/parzibyte
-Facebook: https://facebook.com/parzibyte.fanpage
-Instagram: https://instagram.com/parzibyte
-Hacer una donación vía PayPal: https://paypal.me/LuisCabreraBenito
-------------------------------------------------------------------------------------------------
-*/ ?>
+?>
 <?php
 
-namespace Parzibyte;
+namespace Recetas;
 
-use Parzibyte\BD;
+use Recetas\BD;
 
 
 class Recetas
@@ -46,7 +14,7 @@ class Recetas
 		$bd = BD::obtener();
 		$sentencia = $bd->prepare("SELECT recetas.id, recetas.nombre, recetas.descripcion, recetas.porciones, fotos_recetas.foto
 		FROM recetas INNER JOIN fotos_recetas
-		ON recetas.id = fotos_recetas.id_receta 
+		ON recetas.id = fotos_recetas.id_receta
 		WHERE recetas.nombre LIKE ? OR recetas.descripcion LIKE ?");
 		$sentencia->execute(["%$busqueda%", "%$busqueda%"]);
 		return $sentencia->fetchAll();
